724f54d1b5e30c9337734b0ad964797f6a087307
Imoleayo <imoleayoakinyemi92@gmail.com>
2026-03-18 12:58:49 +0100

    Update feature

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import ServerlessHttp from "serverless-http";
 import { connectToDb } from "./Config/connectToDb.js";
 import todoRouter from "./routers/todo.js";
 import cors from "cors";
@@ -8,14 +9,16 @@ app.use(express.json());
 app.use(cors({
   origin: "*"
 }));
-const port = 4000;
+// const port = 4000;
 connectToDb();
 
-app.listen(port, () => {
-  console.log(`App currently running at port:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`App currently running at port:${port}`);
+// });
 
 app.get("/", (req, res) => {
   res.send("Hello at todo app!!");
 });
 app.use("/api/todo", todoRouter);
+
+export default ServerlessHttp(app)
