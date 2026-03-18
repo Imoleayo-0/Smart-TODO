ba4a9b1faf2ba003226071b0a693889ccd1e5f78
Imoleayo <imoleayoakinyemi92@gmail.com>
2026-03-18 13:12:43 +0100

    Update vercel

--- api/index.jsx
+++ api/index.jsx
@@ -1,4 +1,9 @@
-import ServerlessHttp from "serverless-http";
-import app from "../app";
+// import ServerlessHttp from "serverless-http";
+// import app from "../app";
 
-export default ServerlessHttp(app);
+// export default ServerlessHttp(app);
+
+
+export default function handler(req, res) {
+  res.status(200).json({ message: "API working ✅" });
+}
